3f08610f85bce98f5f8db4d1d7f11e25f3ba21a0
JY <Yoon@spaceigaming.comgit config --global user.email yoon@spaceigaming.com>
2025-11-24 19:54:55 +0900

    hi

---
+++ groupware-front/src/views/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>메인 화면</h1>
+        <p>여기는 그룹웨어 메인 대시보드입니다</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+}
+</script>
+
+<style scoped>
+/* 메인 화면 전용 스타일 */
+</style>
